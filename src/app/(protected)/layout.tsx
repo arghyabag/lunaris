be04e0cbc95a6a058b03ec50eee690793d78ad5c
Arghya <arghyabag81101@gmail.com>
2024-12-07 15:46:01 +0530

    layour and sidebar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { UserButton } from "@clerk/nextjs"
+import App from "next/app"
+import AppSidebar from "./dashboard/app-sidebar"
 
 
 type Props={
@@ -9,6 +11,7 @@ type Props={
 const SidebarLayout =({children}:Props)=>{
     return (
         <SidebarProvider>
+            <AppSidebar/>
             <main className="w-full m-2">
                 <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
                     <div className="ml-auto"></div>
